e291fcda8826b8cf4d2f763c60a8c06749303eb1
kuhpferd <uli.toll@codingcow.de>
2024-09-05 11:18:05 +0100

    update to 0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='cpdfkit',
-    version='0.1.4',
+    version='0.1.5',
     description='A toolkit for rendering HTML to PDF using Chrome headless.',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- src/cpdfkit/__init__.py
+++ src/cpdfkit/__init__.py
@@ -1,1 +1,1 @@
-from .cpdfkit import CPDFKit, generate_pdf, find_chrome, PAPER_SIZESexiyt
+from .cpdfkit import CPDFKit, generate_pdf, find_chrome, PAPER_SIZES
